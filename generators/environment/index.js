dade57dd1b4158def2dac8a656dc09440e0ea30b
Tyler Fisher <tfisher@politico.com>
2018-01-02 14:55:55 -0500

    use pipenv

--- generators/environment/index.js
+++ generators/environment/index.js
@@ -1,6 +1,5 @@
 const Generator = require('yeoman-generator');
 const shell = require('shelljs');
-const chalk = require('chalk');
 
 module.exports = class extends Generator {
   writing() {
@@ -8,18 +7,13 @@ module.exports = class extends Generator {
       this.templatePath('env'),
       this.destinationPath('./.env'));
     this.fs.copy(
-      this.templatePath('requirements.txt'),
-      this.destinationPath('requirements.txt'));
+      this.templatePath('Pipfile'),
+      this.destinationPath('./Pipfile'));
   }
 
   install() {
-    shell.exec('virtualenv venv -p python3');
-  }
-
-  end() {
-    this.log(chalk.cyan('Activate your virtual environment and install requirements to begin developing:'));
-    this.log(chalk.bgGreen('$ source .env'));
-    this.log(chalk.bgGreen('$ pip install -r requirements.txt'));
-    this.log(chalk.cyan('Done and ready to go! 🏁'));
+    shell.exec('pipenv install', {
+      silent: true,
+    });
   }
 };
